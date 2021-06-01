32668bf99f31a0d38586af50b161bcaa5211b23f
Hana897TRX <nemesisdarknes897gt@gmail.com>
2021-06-01 13:05:39 -0500

    DataBase Script

---
+++ scriptBase.sql
@@ -0,0 +1,37 @@
+-- Nombre de la página : NekoBook Store :v / Kawai Book Store / New Gandhi Store / Sakura Book Store /
+CREATE DATABASE proyectoWeb;
+
+USE proyectoWeb;
+
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users(
+    idUser INT auto_increment,
+    name VARCHAR(150) NOT NULL,
+    lastname VARCHAR(150) NOT NULL,
+    birthday DATE NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    password VARCHAR(224),
+    PRIMARY KEY(idUser)
+) ENGINE=INNODB;
+
+DROP TABLE IF EXISTS Author;
+CREATE TABLE Author(
+    idAuthor INT auto_increment,
+    authorName VARCHAR(150),
+    PRIMARY KEY (idAuthor)
+) ENGINE=INNODB;
+
+DROP TABLE IF EXISTS Books;
+CREATE TABLE Books(
+    idBook INT auto_increment,
+    bookName VARCHAR(150) NOT NULL,
+    isbn VARCHAR(15) NOT NULL,
+    fechaCompra DATE NOT NULL,
+    idAuthor INT,
+    status VARCHAR(50),
+    coverBookContent MEDIUMBLOB,
+    coverBookSize double,
+    coverBookType VARCHAR(255),
+    PRIMARY KEY (idBook),
+    FOREIGN KEY (idAuthor) REFERENCES Author(idAuthor)
+) ENGINE=INNODB;
