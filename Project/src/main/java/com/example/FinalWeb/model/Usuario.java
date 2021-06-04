704d81b38e53e43855fb00801fe219b03cc21729
Lizzie264 <57414831+Lizzie264@users.noreply.github.com>
2021-06-04 10:11:49 -0500

    user model

---
+++ Project/src/main/java/com/example/FinalWeb/model/Usuario.java
@@ -0,0 +1,85 @@
+package com.example.FinalWeb.model;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Usuario implements Serializable {
+    private int idUser;
+    private String name;
+    private String lastName;
+    private Date birthday;
+    private String email;
+    private String password;
+
+    public Usuario() {
+    }
+
+    public Usuario(int idUser, String name, String lastName, Date birthday, String email, String password) {
+        this.idUser = idUser;
+        this.name = name;
+        this.lastName = lastName;
+        this.birthday = birthday;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "idUser=" + idUser +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthday=" + birthday +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
